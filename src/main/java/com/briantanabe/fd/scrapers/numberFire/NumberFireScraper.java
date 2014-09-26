ae22072d4218c8735477a402b5adf1b9be2f170d
Brian Tanabe <btanabe@zebra.com>
2014-09-26 17:10:34 -0500

    Stubbed out tests.  I think I have to rethink how I'm going to handle the objects containing data and how to merge these.

--- src/main/java/com/briantanabe/fd/scrapers/numberFire/NumberFireScraper.java
+++ src/main/java/com/briantanabe/fd/scrapers/numberFire/NumberFireScraper.java
@@ -1,9 +1,17 @@
 package com.briantanabe.fd.scrapers.numberFire;
 
 import com.briantanabe.fd.fantasy.player.NumberFireRanking;
+import com.briantanabe.fd.scrapers.numberFire.positions.CurrentWeekNumberFireHtmlScraper;
+import com.briantanabe.fd.scrapers.numberFire.positions.NumberFirePositionScraper;
+import com.briantanabe.fd.scrapers.numberFire.positions.RemainingSeasonNumberFireJsonScraper;
+import com.briantanabe.fd.web.WebPage;
 import com.briantanabe.fd.web.WebRequest;
+import org.jsoup.nodes.Document;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * Created by BTanabe on 9/26/2014.
@@ -15,8 +23,41 @@ public class NumberFireScraper {
         this.webRequest = webRequest;
     }
 
-    public ArrayList<NumberFireRanking> scrape(){
+    public ArrayList<NumberFireRanking> scrape() throws IOException {
         ArrayList<NumberFireRanking> players = new ArrayList<NumberFireRanking>(2000);
+
+        players.addAll(scrapeDefensePlayerProjections());
+
         return players;
+    }
+
+    private ArrayList<NumberFireRanking> scrapeDefensePlayerProjections() throws IOException {
+        ArrayList<NumberFireRanking> currentWeekProjections = getPlayerProjections(webRequest.getPageAsDocument(WebPage.NUMBER_FIRE_CURRENT_WEEK_DEFENSE_PROJECTIONS_URL), new CurrentWeekNumberFireHtmlScraper());
+        ArrayList<NumberFireRanking> remainingSeasonProjections = getPlayerProjections(webRequest.getPageAsDocument(WebPage.NUMBER_FIRE_REMAINING_SEASON_DEFENSE_PROJECTIONS_URL), new RemainingSeasonNumberFireJsonScraper());
+
+        return mergeCurrentWeekProjectionsAndRemainingSeasonProjections(currentWeekProjections, remainingSeasonProjections);
+    }
+
+    private ArrayList<NumberFireRanking> getPlayerProjections(Document page, NumberFirePositionScraper positionScraper){
+        NumberFirePageScraper scraper = new NumberFirePageScraper(page, positionScraper);
+        scraper.scrape();
+        return scraper.getPlayerRankings();
+    }
+
+    private ArrayList<NumberFireRanking> mergeCurrentWeekProjectionsAndRemainingSeasonProjections(ArrayList<NumberFireRanking> currentWeekProjections, ArrayList<NumberFireRanking> remainingSeasonProjections){
+        NumberFireRanking[] currentWeekProjectionsArray = currentWeekProjections.toArray(new NumberFireRanking[currentWeekProjections.size()]);
+
+        // Favor remaining season projections becuase they contain the ESPN & Yahoo player IDs
+        for(NumberFireRanking teamRemainingSeasonProjection : remainingSeasonProjections){
+            int index = Arrays.binarySearch(currentWeekProjectionsArray, teamRemainingSeasonProjection.getNumberFireId(), new Comparator<Object>() {
+                public int compare(Object lhs, Object rhs) {
+                    return ((NumberFireRanking) lhs).getNumberFireId() - ((NumberFireRanking) rhs).getNumberFireId();
+                }
+            });
+
+            NumberFireRanking currentWeekProjection = currentWeekProjectionsArray[index];
+        }
+
+        return null;
     }
 }
--- src/test/java/com/briantanabe/fd/unitTests/scrapers/numberFire/NumberFireScraperTests.java
+++ src/test/java/com/briantanabe/fd/unitTests/scrapers/numberFire/NumberFireScraperTests.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 /**
  * Created by BTanabe on 9/26/2014.
@@ -16,9 +17,14 @@ public class NumberFireScraperTests {
 
     @Test
     public void shouldBeAbleToFindTwoThousandPlayers(){
-        NumberFireScraper scraper = new NumberFireScraper(new WebRequest());
-        ArrayList<NumberFireRanking> players = scraper.scrape();
+        try {
+            NumberFireScraper scraper = new NumberFireScraper(new WebRequest());
+            ArrayList<NumberFireRanking> players = scraper.scrape();
 
-        assertEquals("Did not find the correct number of players", 2000, players.size());
+            assertEquals("Did not find the correct number of players", 2000, players.size());
+            fail("I HAVEN'T IMPLEMENTED THIS YET");
+        } catch(Exception ex){
+            fail("FAILED to parse all numberFire player projections correctly");
+        }
     }
 }
