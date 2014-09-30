92d408722e7a89272c15b7d208f373823389f16f
Brian Tanabe <btanabe@zebra.com>
2014-09-30 16:41:49 -0500

    Committing to save the URL for the tutorial I'm using.

--- DatabaseUpdater/test/java/com/briantanabe/fd/du/unitTests/DatabaseUpdaterTests.java
+++ DatabaseUpdater/test/java/com/briantanabe/fd/du/unitTests/DatabaseUpdaterTests.java
@@ -2,13 +2,19 @@ package com.briantanabe.fd.du.unitTests;
 
 import org.junit.Test;
 
+import static junit.framework.TestCase.fail;
+
 /**
  * Created by BTanabe on 9/30/2014.
  */
 public class DatabaseUpdaterTests {
 
+
+    // http://docs.jboss.org/hibernate/orm/4.2/quickstart/en-US/html/ch03.html
+    // http://www.tutorialspoint.com/hibernate/hibernate_configuration.htm
     @Test
     public void testCanReachDatabase(){
-        
+
+        fail("Cannot reach database");
     }
 }
