c60691f4e59b5b97bfb58be1e205123fe5db5174
Brian Tanabe <btanabe@zebra.com>
2014-09-30 13:43:26 -0500

    Added implementing DataAccess tests.  Fixed dependency files.  I still would like to see if I can make a "global" dependency file.

--- DatabaseUpdater/src/java/com/briantanabe/fd/du/updater/DatabaseUpdater.java
+++ DatabaseUpdater/src/java/com/briantanabe/fd/du/updater/DatabaseUpdater.java
@@ -4,4 +4,6 @@ package com.briantanabe.fd.du.updater;
  * Created by BTanabe on 9/30/2014.
  */
 public class DatabaseUpdater {
+
+
 }
--- DatabaseUpdater/test/java/com/briantanabe/fd/du/unitTests/DataAccessTests.java
+++ DatabaseUpdater/test/java/com/briantanabe/fd/du/unitTests/DataAccessTests.java
@@ -1,8 +1,15 @@
 package com.briantanabe.fd.du.unitTests;
 
+import com.briantanabe.fd.fantasy.player.NflPlayer;
+import com.briantanabe.fd.providers.PlayerIdProvider;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by BTanabe on 9/30/2014.
@@ -10,7 +17,30 @@ import static org.junit.Assert.fail;
 public class DataAccessTests {
 
     @Test
-    public void firstTest(){
-        fail("I CAN FAIL!");
+    public void shouldBeAbleToUseEspnLeaguePlayerOwnershipProvider(){
+        fail("NOT YET IMPLEMENTED");
+    }
+
+    @Test
+    public void shouldBeAbleToUseNumberFireCurrentWeekProjectionsProvider(){
+        fail("NOT YET IMPLEMENTED");
+    }
+
+    @Test
+    public void shouldBeAbleToUseNumberFireRemainingSeasonProjectionsProvider(){
+        fail("NOT YET IMPLEMENTED");
+    }
+
+    @Test
+    public void shouldBeAbleToUseThePlayerIdProvider(){
+        PlayerIdProvider playerIdProvider = mock(PlayerIdProvider.class);
+
+        ArrayList<NflPlayer> players = new ArrayList<NflPlayer>();
+        players.add(new NflPlayer(1000, 2000, "Brian Tanabe"));
+        players.add(new NflPlayer(1001, 2001, "Benjamin Coutrney"));
+        players.add(new NflPlayer(1002, 2002, "Samuel Courtney"));
+        when(playerIdProvider.getAllPlayersAsArrayList()).thenReturn(players);
+
+        assertEquals("Was not able to use PlayerIdProvider", 3, players.size());
     }
 }
