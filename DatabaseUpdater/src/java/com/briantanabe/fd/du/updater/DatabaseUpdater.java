003c9e7bd3f09ba1add3d8d4ed0131f1cd1798c6
Brian Tanabe <btanabe@zebra.com>
2014-09-30 13:30:18 -0500

    Stubbing out new test and class for the DatabaseUpdater module.

---
+++ DatabaseUpdater/src/java/com/briantanabe/fd/du/updater/DatabaseUpdater.java
@@ -0,0 +1,7 @@
+package com.briantanabe.fd.du.updater;
+
+/**
+ * Created by BTanabe on 9/30/2014.
+ */
+public class DatabaseUpdater {
+}
---
+++ DatabaseUpdater/test/java/com/briantanabe/fd/du/unitTests/DataAccessTests.java
@@ -0,0 +1,16 @@
+package com.briantanabe.fd.du.unitTests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Created by BTanabe on 9/30/2014.
+ */
+public class DataAccessTests {
+
+    @Test
+    public void firstTest(){
+        fail("I CAN FAIL!");
+    }
+}
