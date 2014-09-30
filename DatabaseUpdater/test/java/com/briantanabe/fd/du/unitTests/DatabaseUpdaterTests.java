4c4d7a172cb31400d8c71e431d8ba27564c2aa5f
Brian Tanabe <btanabe@zebra.com>
2014-09-30 16:15:08 -0500

    Stubbing out empty tests for reaching the database.

---
+++ DatabaseUpdater/test/java/com/briantanabe/fd/du/unitTests/DatabaseUpdaterTests.java
@@ -0,0 +1,14 @@
+package com.briantanabe.fd.du.unitTests;
+
+import org.junit.Test;
+
+/**
+ * Created by BTanabe on 9/30/2014.
+ */
+public class DatabaseUpdaterTests {
+
+    @Test
+    public void testCanReachDatabase(){
+        
+    }
+}
