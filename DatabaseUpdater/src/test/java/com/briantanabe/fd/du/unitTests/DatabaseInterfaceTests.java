25f68dae090dc95d971cf956ddb61eec33eea2cc
Brian Tanabe <btanabe@zebra.com>
2014-10-01 14:42:53 -0500

    Added a static helper function to turn logging off/set the logging level.

--- DatabaseUpdater/src/test/java/com/briantanabe/fd/du/unitTests/DatabaseInterfaceTests.java
+++ DatabaseUpdater/src/test/java/com/briantanabe/fd/du/unitTests/DatabaseInterfaceTests.java
@@ -1,9 +1,11 @@
 package com.briantanabe.fd.du.unitTests;
 
+import com.briantanabe.fd.du.log.LoggingUtility;
 import com.briantanabe.fd.du.updater.DatabaseInterface;
 import com.briantanabe.fd.fantasy.player.NflPlayer;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -17,6 +19,11 @@ import static junit.framework.TestCase.assertTrue;
  */
 public class DatabaseInterfaceTests {
     private static DatabaseInterface updater;
+
+    @BeforeClass
+    public static void turnLoggingOff(){
+        LoggingUtility.turnLoggingOff();
+    }
 
     @Before
     public void setup(){
@@ -44,17 +51,38 @@ public class DatabaseInterfaceTests {
 
         ArrayList<NflPlayer> playersFromDatabase = updater.getAllNflPlayersInDatabase();
         assertEquals("Did not find the correct number of players in the database", playersToAdd.size(), playersFromDatabase.size());
+
         assertEquals("Player name does not match", playerOne.getName(), findPlayerByPlayerName(playerOne.getName(), playersFromDatabase).getName());
+        assertEquals("Player ESPN Id does not match", playerOne.getEspnPlayerId(), findPlayerByPlayerName(playerOne.getName(), playersFromDatabase).getEspnPlayerId());
+        assertEquals("Player numberFire ID does not match", playerOne.getNumberFireId(), findPlayerByPlayerName(playerOne.getName(), playersFromDatabase).getNumberFireId());
     }
 
     @Test
     public void shouldBeAbleToAddMultiplePlayers(){
+        NflPlayer playerOne = new NflPlayer(1000, 2000, "Brian Tanabe");
+        NflPlayer playerTwo = new NflPlayer(1001, 2001, "Benjamin Courtney");
+        NflPlayer playerThree = new NflPlayer(1002, 2002, "Samuel Courtney");
+
         ArrayList<NflPlayer> playersToAdd = new ArrayList<NflPlayer>();
-        playersToAdd.add(new NflPlayer(1000, 2000, "Brian Tanabe"));
-        playersToAdd.add(new NflPlayer(1001, 2001, "Benjamin Courtney"));
-        playersToAdd.add(new NflPlayer(1002, 2002, "Samuel Courtney"));
+        playersToAdd.add(playerOne);
+        playersToAdd.add(playerTwo);
+        playersToAdd.add(playerThree);
 
         updater.insert(playersToAdd);
-        assertEquals("Did not find the correct number of players in the database", playersToAdd.size(), updater.getAllNflPlayersInDatabase().size());
+
+        ArrayList<NflPlayer> playersFromDatabase = updater.getAllNflPlayersInDatabase();
+        assertEquals("Did not find the correct number of players in the database", playersToAdd.size(), playersFromDatabase.size());
+
+        assertEquals("PlayerOne's name does not match", playerOne.getName(), findPlayerByPlayerName(playerOne.getName(), playersFromDatabase).getName());
+        assertEquals("PlayerOne's ESPN Id does not match", playerOne.getEspnPlayerId(), findPlayerByPlayerName(playerOne.getName(), playersFromDatabase).getEspnPlayerId());
+        assertEquals("PlayerOne's numberFire ID does not match", playerOne.getNumberFireId(), findPlayerByPlayerName(playerOne.getName(), playersFromDatabase).getNumberFireId());
+
+        assertEquals("PlayerTwo's name does not match", playerTwo.getName(), findPlayerByPlayerName(playerTwo.getName(), playersFromDatabase).getName());
+        assertEquals("PlayerTwo's ESPN Id does not match", playerTwo.getEspnPlayerId(), findPlayerByPlayerName(playerTwo.getName(), playersFromDatabase).getEspnPlayerId());
+        assertEquals("PlayerTwo's numberFire ID does not match", playerTwo.getNumberFireId(), findPlayerByPlayerName(playerTwo.getName(), playersFromDatabase).getNumberFireId());
+
+        assertEquals("PlayerThree's name does not match", playerThree.getName(), findPlayerByPlayerName(playerThree.getName(), playersFromDatabase).getName());
+        assertEquals("PlayerThree's ESPN Id does not match", playerThree.getEspnPlayerId(), findPlayerByPlayerName(playerThree.getName(), playersFromDatabase).getEspnPlayerId());
+        assertEquals("PlayerThree's numberFire ID does not match", playerThree.getNumberFireId(), findPlayerByPlayerName(playerThree.getName(), playersFromDatabase).getNumberFireId());
     }
 }
