dd103d467bfa61a5afd0c53c9351d6f8e52be869
Brian Tanabe <btanabe@zebra.com>
2014-10-13 15:11:17 -0500

    Stubbed out a failing test.  About to implement the PlayerSearchTableRowDataProvider.

---
+++ DataProvider/src/main/java/com/briantanabe/fd/dp/fantasy/player/PlayerSearchTableRow.java
@@ -0,0 +1,12 @@
+package com.briantanabe.fd.dp.fantasy.player;
+
+/**
+ * Created by btanabe on 10/13/2014.
+ */
+public class PlayerSearchTableRow {
+    // Name:
+    // Team:
+    // Position:
+    // NumberFire Current Week Projection:
+    // NumberFire Remaining Season Projection:
+}
---
+++ DataProvider/src/main/java/com/briantanabe/fd/dp/providers/PlayerSearchTableDataProvider.java
@@ -0,0 +1,17 @@
+package com.briantanabe.fd.dp.providers;
+
+import com.briantanabe.fd.dp.fantasy.player.PlayerSearchTableRow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by btanabe on 10/13/2014.
+ */
+public class PlayerSearchTableDataProvider {
+
+    public List<PlayerSearchTableRow> getAllPlayerSearchTableRows(){
+
+        return new ArrayList<PlayerSearchTableRow>();
+    }
+}
