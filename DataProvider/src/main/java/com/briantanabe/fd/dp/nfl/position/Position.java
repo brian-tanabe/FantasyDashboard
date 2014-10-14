6fd5372121a35ded56ebace8d89df634c5f90ec6
Brian Tanabe <btanabe2@gmail.com>
2014-10-13 22:21:08 -0500

    Added position enum and a factory class to set it

---
+++ DataProvider/src/main/java/com/briantanabe/fd/dp/nfl/position/Position.java
@@ -0,0 +1,14 @@
+package com.briantanabe.fd.dp.nfl.position;
+
+/**
+ * Created by Brian on 10/13/14.
+ */
+public enum Position {
+    QUARTERBACK,
+    RUNNING_BACK,
+    WIDE_RECEIVER,
+    TIGHT_END,
+    KICKER,
+    DEFENSE,
+    UNKNOWN;
+}
---
+++ DataProvider/src/main/java/com/briantanabe/fd/dp/nfl/position/PositionFactory.java
@@ -0,0 +1,25 @@
+package com.briantanabe.fd.dp.nfl.position;
+
+/**
+ * Created by Brian on 10/13/14.
+ */
+public class PositionFactory {
+    
+    public static Position whatPosition(String positionString) {
+        positionString = positionString.toLowerCase();
+        if (positionString.contains("qb") || positionString.contains("quarterback"))
+            return Position.QUARTERBACK;
+        else if (positionString.contains("rb") || positionString.contains("running back") || positionString.contains("tailback") || positionString.contains("tb") || positionString.contains("fb") || positionString.contains("fullback"))
+            return Position.RUNNING_BACK;
+        else if (positionString.contains("wide receiver") || positionString.contains("wr") || positionString.contains("wideout"))
+            return Position.WIDE_RECEIVER;
+        else if (positionString.contains("tight end") || positionString.contains("te"))
+            return Position.TIGHT_END;
+        else if (positionString.contains("kicker") || positionString.contains("k") || positionString.contains("pk"))
+            return Position.KICKER;
+        else if (positionString.contains("defense") || positionString.contains("def") || positionString.contains("special teams") || positionString.contains("d/st"))
+            return Position.DEFENSE;
+        else
+            return Position.UNKNOWN;
+    }
+}
