a55ad9163089645b89c598eb602f569e6eec698e
Brian Tanabe <btanabe2@gmail.com>
2014-10-14 22:54:01 -0500

    Implemented failing tests.

--- DatabaseUpdater/src/test/java/com/briantanabe/fd/du/tests/unit/PlayerPositionAndTeamDatabaseTests.java
+++ DatabaseUpdater/src/test/java/com/briantanabe/fd/du/tests/unit/PlayerPositionAndTeamDatabaseTests.java
@@ -1,6 +1,8 @@
 package com.briantanabe.fd.du.tests.unit;
 
 import com.briantanabe.fd.dp.fantasy.player.NflPlayerPositionAndTeam;
+import com.briantanabe.fd.dp.nfl.position.Position;
+import com.briantanabe.fd.dp.nfl.team.NflTeam;
 import com.briantanabe.fd.dp.providers.PlayerPositionAndTeamProvider;
 import com.briantanabe.fd.dp.tests.fixtures.MockWebRequest;
 import com.briantanabe.fd.dp.tests.unit.scrapers.PlayerFinder;
@@ -21,6 +23,12 @@ import static org.junit.Assert.assertEquals;
 public class PlayerPositionAndTeamDatabaseTests {
     private static DatabaseAccessor accessor = new DatabaseAccessor();
     private static DatabaseUpdater updater = new DatabaseUpdater();
+    private static NflPlayerPositionAndTeam playerFromDatabase;
+
+    private static final String PLAYER_NAME = "Andrew Luck";
+    private static final int PLAYER_ESPN_ID = 14874;
+    private static final NflTeam PLAYER_NFL_TEAM = NflTeam.COLTS;
+    private static final Position PLAYER_POSITION = Position.QUARTERBACK;
 
     @BeforeClass
     public static void setup() {
@@ -31,6 +39,8 @@ public class PlayerPositionAndTeamDatabaseTests {
             playerPositionAndTeamProvider.scrapeForPlayerPositionsAndTeams();
             List<NflPlayerPositionAndTeam> playerPositionAndTeamRows = playerPositionAndTeamProvider.getAllPlayerPositionsAndTeams();
             updater.insert(playerPositionAndTeamRows);
+
+            playerFromDatabase = PlayerFinder.findPlayerByPlayerName(PLAYER_NAME, accessor.getAllPlayerPositionAndTeamObjectsFromTheDatabase());
         } catch (Exception ex) {
             ex.printStackTrace();
             fail("Failed to create the PlayerPositionAndTeam table");
@@ -39,9 +49,21 @@ public class PlayerPositionAndTeamDatabaseTests {
 
     @Test
     public void shouldBeAbleToStoreAndRecallPlayersName(){
-        final String playerName = "Andrew Luck";
-        List<NflPlayerPositionAndTeam> playerPositionAndTeams = accessor.getAllPlayerPositionAndTeamObjectsFromTheDatabase();
-        NflPlayerPositionAndTeam playerFromDatabase = PlayerFinder.findPlayerByPlayerName(playerName, playerPositionAndTeams);
-        assertEquals("Unable to find Peyton Manning by name", playerName, playerFromDatabase.getName());
+        assertEquals(String.format("Unable to find %s by name", PLAYER_NAME), PLAYER_NAME, playerFromDatabase.getName());
+    }
+
+    @Test
+    public void shouldBeAbleToStoreAndRecallPlayersEspnId(){
+        assertEquals(String.format("Unable to find %s by name", PLAYER_NAME), PLAYER_ESPN_ID, playerFromDatabase.getEspnPlayerId());
+    }
+
+    @Test
+    public void shouldBeAbleToStoreAndRecallPlayersNflTeam(){
+        assertEquals(String.format("Unable to find %s by name", PLAYER_NAME), PLAYER_NFL_TEAM, playerFromDatabase.getNflTeam());
+    }
+
+    @Test
+    public void shouldBeAbleToStoreAndRecallPlayersPositionCorrectly(){
+        assertEquals(String.format("Unable to find %s by name", PLAYER_NAME), PLAYER_POSITION, playerFromDatabase.getAllEligiblePositions().get(0));
     }
 }
